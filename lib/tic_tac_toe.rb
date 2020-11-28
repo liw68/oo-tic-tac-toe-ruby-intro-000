3f52ed951db697f7522ddc93e06e1856c482c191
Liam Walsh <liw6800@gmail.com>
2020-11-28 01:12:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,8 +39,8 @@ WIN_COMBINATIONS = [
     end
   end
 
-  def valid_move?(board, index)
-    if index.between?(0, 8) && !position_taken?(board, index)
+  def valid_move?(index)
+    if index.between?(0, 8) && !position_taken?(index)
       return true
     else
       return false
